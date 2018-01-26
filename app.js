933550dd5868ea10b877c62987b50d55007cbb95
jquattlebaum <jamesquattlebaum@spsk12.net>
2018-01-26 10:09:50 -0500

    service js file add
    added service worker js files

---
+++ app.js
@@ -0,0 +1,22 @@
+﻿console.log('Start Service!');
+
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+        .register('/jquattlebaum.github.io/service-worker.js')
+        .then(function () { console.log('Service Worker Registered'); });
+}
+
+
+/*
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js', { scope: '/timeclock.sps.k12.va.us/' })
+        .then(function (reg) {
+            console.log('Success! Scope is ' + reg.scope);
+        }).catch(function (error) {
+            console.log('Failed with ' + error);
+        });
+}
+*/
+
+console.log('End Service!');
